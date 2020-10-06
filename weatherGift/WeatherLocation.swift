f0d00ae32d9576f81111e46f95b9475cf5c7b950
Chris Bertram <bertramc@bc.edu>
2020-10-06 16:52:55 -0400

    6.2 complete

---
+++ weatherGift/WeatherLocation.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherLocation.swift
+//  weatherGift
+//
+//  Created by Chris Bertram on 10/6/20.
+//
+
+import Foundation
+
+struct WeatherLocation {
+    var name: String
+    var latitude: Double
+    var longitude: Double
+}

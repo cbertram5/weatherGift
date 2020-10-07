6e983b0910350c9789a9d39bed1ae8c3fe24ecd8
Chris Bertram <bertramc@bc.edu>
2020-10-07 18:59:49 -0400

    with .gitignore

---
+++ weatherGift/APIKeys.swift
@@ -0,0 +1,13 @@
+//
+//  APIKeys.swift
+//  weatherGift
+//
+//  Created by Chris Bertram on 10/7/20.
+//
+
+import Foundation
+
+struct APIKeys {
+    static let googlePlacesKey = "AIzaSyCEV1tvWjMYg2ipobKKbIhCiqPb0X4n4IU"
+}
+

f82bd1b26498e6e3a2a0be6232d55e991fd2ae28
Chris Bertram <bertramc@bc.edu>
2020-10-21 16:58:29 -0400

    6.15 complete

---
+++ weatherGift/DailyTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  DailyTableViewCell.swift
+//  weatherGift
+//
+//  Created by Chris Bertram on 10/21/20.
+//
+
+import UIKit
+
+class DailyTableViewCell: UITableViewCell {
+    @IBOutlet weak var dailyImageView: UIImageView!
+    @IBOutlet weak var dailyWeekdayLabel: UILabel!
+    @IBOutlet weak var dailyHighLabel: UILabel!
+    @IBOutlet weak var dailySummaryLabel: UITextView!
+    @IBOutlet weak var dailyLowLabel: UILabel!
+    
+    var dailyWeather: DailyWeather! {
+        didSet {
+            dailyImageView.image = UIImage(named: dailyWeather.dailyIcon)
+            dailyWeekdayLabel.text = dailyWeather.dailyWeekday
+            dailySummaryLabel.text = dailyWeather.dailySummary
+            dailyHighLabel.text = "\(dailyWeather.dailyHigh)°"
+            dailyLowLabel.text = "\(dailyWeather.dailyLow)°"
+        }
+    }
+}

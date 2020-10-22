26c81947751637061983dad610c86cb15de184da
Chris Bertram <bertramc@bc.edu>
2020-10-22 15:47:11 -0400

    6.17 complete

---
+++ weatherGift/HourlyCollectionViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  HourlyCollectionViewCell.swift
+//  weatherGift
+//
+//  Created by Chris Bertram on 10/22/20.
+//
+
+import UIKit
+
+class HourlyCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var hourLabel: UILabel!
+    @IBOutlet weak var hourlyTemperature: UILabel!
+    @IBOutlet weak var iconImageView: UIImageView!
+    
+    var hourlyWeather: HourlyWeather! {
+        didSet {
+            hourLabel.text = hourlyWeather.hour
+            iconImageView.image = UIImage(systemName: hourlyWeather.hourlyIcon)
+            hourlyTemperature.text = "\(hourlyWeather.hourlyTemperature)°"
+        }
+    }
+}
